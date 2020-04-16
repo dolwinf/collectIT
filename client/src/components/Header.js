bc5535d92c91f93b1f3f53b48f4a1e0e15d6f66e
shell <dolwinf@gmail.com>
2020-04-16 23:32:29 +1000

    Added conditional rendering logic for menu items

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,17 +7,9 @@ function Header() {
   const [activeItem, setActiveItem] = useState("home");
 
   const handleItemClick = (name) => setActiveItem({ activeItem: name });
-
+  let user = true;
   return (
-    <Menu
-      color="blue"
-      stackable
-      fluid
-      inverted
-      position="center"
-      size="large"
-      id="menu-text"
-    >
+    <Menu color="blue" stackable fluid inverted size="large" id="menu-text">
       <Link to="/">
         <Menu.Item
           header
@@ -25,59 +17,72 @@ function Header() {
           active={activeItem === "home"}
           onClick={handleItemClick}
         >
-          <Image size="mini" src={logo} style={{ marginRight: "1em" }} />
+          <Image size="mini" src={logo} style={{ marginRight: "2em" }} />
           <strong>
             <i>CollectIT</i>
           </strong>
         </Menu.Item>
       </Link>
-      <Container text>
-        <Link to="/create">
+      {user && (
+        <Container text>
+          <Menu.Menu position="left">
+            <Link to="/create">
+              <Menu.Item
+                name="create"
+                active={activeItem === "create"}
+                onClick={handleItemClick}
+              />
+            </Link>
+            <Link to="/account">
+              <Menu.Item
+                name="account"
+                active={activeItem === "account"}
+                onClick={handleItemClick}
+              />
+            </Link>
+            <Link to="/settings">
+              <Menu.Item
+                name="settings"
+                active={activeItem === "settings"}
+                onClick={handleItemClick}
+              />
+            </Link>
+          </Menu.Menu>
+        </Container>
+      )}
+
+      {!user && (
+        <Container text>
+          <Menu.Menu position="right">
+            <Link to="/login">
+              <Menu.Item
+                name="login"
+                active={activeItem === "login"}
+                onClick={handleItemClick}
+              />
+            </Link>
+            <Link to="/register">
+              <Menu.Item
+                name="register"
+                active={activeItem === "register"}
+                onClick={handleItemClick}
+              />
+            </Link>
+          </Menu.Menu>
+        </Container>
+      )}
+      {user && (
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Input icon="search" placeholder="Search Asset ID..." />
+          </Menu.Item>
           <Menu.Item
-            name="create"
-            active={activeItem === "create"}
+            name="logout"
+            active={activeItem === "logout"}
             onClick={handleItemClick}
           />
-        </Link>
-        <Link to="/account">
-          <Menu.Item
-            name="account"
-            active={activeItem === "account"}
-            onClick={handleItemClick}
-          />
-        </Link>
-        <Link to="/settings">
-          <Menu.Item
-            name="settings"
-            active={activeItem === "settings"}
-            onClick={handleItemClick}
-          />
-        </Link>
-        <Link to="/login">
-          <Menu.Item
-            name="login"
-            active={activeItem === "login"}
-            onClick={handleItemClick}
-          />
-        </Link>
-        <Link to="/register">
-          <Menu.Item
-            name="register"
-            active={activeItem === "register"}
-            onClick={handleItemClick}
-          />
-        </Link>
-      </Container>
-      <Menu.Menu position="right">
-        <Menu.Item>
-          <Input icon="search" placeholder="Search Asset ID..." />
-        </Menu.Item>
-        <Menu.Item
-          name="logout"
-          active={activeItem === "logout"}
-          onClick={handleItemClick}
-        />
-      </Menu.Menu>
+        </Menu.Menu>
+      )}
     </Menu>
   );
 }
